3e9da5c0875141bf2245138058f7283e310ea996
fares <fares.haliim@gmail.com>
2024-04-06 19:16:33 +0200

    convert from regular function to arrow function

--- src/AppLayout.js
+++ src/AppLayout.js
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 
-function AppLayout() {
+const AppLayout = () => {
   return (
     <main>
       <Header />
       <Outlet />
     </main>
   );
-}
+};
 
 export default AppLayout;
